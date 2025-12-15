15ae8a44540cfddddc4fe5268031b5763549b9ad
Krish-shah05 <246282215+Krish-shah05@users.noreply.github.com>
2025-12-15 12:27:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+// import express
 const express = require("express");
 const cors = require("cors");
 
+// create app
 const app = express();
 const PORT = 5000;
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// GET API
+// simple GET API
 app.get("/api/users", (req, res) => {
   res.json([
     { id: 1, name: "Krish" },
@@ -17,7 +19,7 @@ app.get("/api/users", (req, res) => {
   ]);
 });
 
-// Start Server
+// start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log("Backend running on http://localhost:5000");
 });
